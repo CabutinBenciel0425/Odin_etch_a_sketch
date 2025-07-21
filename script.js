f39795b12dca56f58d304d3c7ba786a5f700acf3
Benciel Cabutin <cabutinb@gmail.com>
2025-07-21 14:26:38 +0800

    added a reset, randomize color and opacity button

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const mainDiv = document.createElement('div')
 const btnAlert = document.getElementById('btnAlert')
+const diffColor = document.getElementById('diffColor')
+const diffOpacity = document.getElementById('diffOpacity')
+const arrColor = ['red', 'blue', 'yellow', 'green', 'orange', 'white', 'grey', 'brown', 'beige', 'black']
+const btnReset = document.getElementById('reset')
+
+let drawMode = "default"
+
 document.body.appendChild(mainDiv);
 mainDiv.classList.add('mainDiv');
 
@@ -10,8 +17,25 @@ function gridMaker(columns, rows, columnSize, rowSize){
     for(let i = 1; i <= columns; i++){
         for(let j = 1; j <= rows; j++){
             const childDiv = document.createElement('div')
+            childDiv.dataset.opacity = "0";
+            
             childDiv.addEventListener('mouseover', () => {
-                childDiv.style.backgroundColor = '#696969';
+                if(drawMode === "default"){
+                    childDiv.style.backgroundColor = '#696969';
+                }
+                else if(drawMode === "color"){
+                    childDiv.style.backgroundColor = randomColor();
+                }
+                else{
+                    let current = parseFloat(childDiv.dataset.opacity);
+                    if (current < 10) {
+                        current += 1;
+                        childDiv.dataset.opacity = current.toString();
+                        const newOpacity = current / 10;
+                        childDiv.style.backgroundColor = `rgba(0, 0, 0, ${newOpacity})`;
+                    }
+                }
+                
             })
             mainDiv.appendChild(childDiv);
             childDiv.classList.add('childDiv');
@@ -29,3 +53,23 @@ btnAlert.addEventListener('click', () => {
     console.log(columns, rows)
     gridMaker(columns, rows, columnSize, rowSize);
 })
+
+
+diffColor.addEventListener('click', () => {
+  drawMode = "color";
+});
+
+diffOpacity.addEventListener('click', () => {
+  drawMode = "opacity";
+});
+
+
+const randomColor = () => {
+    const color = arrColor[Math.floor(Math.random() * arrColor.length)]
+    return color;
+}
+
+btnReset.addEventListener('click', () => {
+  mainDiv.innerHTML = "";
+  drawMode = "default"
+});
